1bc4157044594097ec1f1b13d34537f4a3a1f423
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-19 14:34:28 +0330

    created routers page and home-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/modules/Navbar/Navbar"
-
+import { useRoutes } from "react-router-dom"
+import routes from "./routes"
 
 function App() {
+  const router = useRoutes(routes)
   return (
     <>
       <Navbar/>
+      {router}
     </>
   )
 }
