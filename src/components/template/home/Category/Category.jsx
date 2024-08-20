2b0c0abae4d78c0d5990b7ea0c6b4b2a907ad713
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-20 14:43:30 +0330

    fixed style category for home-page

--- src/components/template/home/Category/Category.jsx
+++ src/components/template/home/Category/Category.jsx
@@ -1,5 +1,6 @@
 import './Category.css'
 import Title from '../../../modules/Title/Title'
+import { Link } from 'react-router-dom'
 
 function Category() {
   return (
@@ -12,60 +13,60 @@ function Category() {
             </div>
             <div className="row mt-3">
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <div className="category_item">
+                    <Link to='/' className="category_item">
                         <img src="/images/cate1.jpg" alt="category image" className="category_item_img" />
                         <h6 className="category_item_title">قهوه فوری</h6>
                         <span className="category_item_number">128</span>
-                    </div>
+                    </Link>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <div className="category_item">
+                    <Link to='/' className="category_item">
                         <img src="/images/cate1.jpg" alt="category image" className="category_item_img" />
                         <h6 className="category_item_title">قهوه فوری</h6>
                         <span className="category_item_number">128</span>
-                    </div>
+                    </Link>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <div className="category_item">
+                    <Link to='/' className="category_item">
                         <img src="/images/cate1.jpg" alt="category image" className="category_item_img" />
                         <h6 className="category_item_title">قهوه فوری</h6>
                         <span className="category_item_number">128</span>
-                    </div>
+                    </Link>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <div className="category_item">
+                    <Link to='/' className="category_item">
                         <img src="/images/cate1.jpg" alt="category image" className="category_item_img" />
                         <h6 className="category_item_title">قهوه فوری</h6>
                         <span className="category_item_number">128</span>
-                    </div>
+                    </Link>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <div className="category_item">
+                    <Link to='/' className="category_item">
                         <img src="/images/cate1.jpg" alt="category image" className="category_item_img" />
                         <h6 className="category_item_title">قهوه فوری</h6>
                         <span className="category_item_number">128</span>
-                    </div>
+                    </Link>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <div className="category_item">
+                    <Link to='/' className="category_item">
                         <img src="/images/cate1.jpg" alt="category image" className="category_item_img" />
                         <h6 className="category_item_title">قهوه فوری</h6>
                         <span className="category_item_number">128</span>
-                    </div>
+                    </Link>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <div className="category_item">
+                    <Link to='/' className="category_item">
                         <img src="/images/cate1.jpg" alt="category image" className="category_item_img" />
                         <h6 className="category_item_title">قهوه فوری</h6>
                         <span className="category_item_number">128</span>
-                    </div>
+                    </Link>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <div className="category_item">
+                    <Link to='/' className="category_item">
                         <img src="/images/cate1.jpg" alt="category image" className="category_item_img" />
                         <h6 className="category_item_title">قهوه فوری</h6>
                         <span className="category_item_number">128</span>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </div>
