562ebc6d2fb80236cb340c6e264a2d8a6630a19a
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-21 14:54:04 +0330

    created page Location

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,9 @@
 import Home from "./page/Home/Home";
+import Location from "./page/location/Location";
 
 const routes = [
     {path:"/" , element:<Home/>},
+    {path:"/Location" , element:<Location/>},
 ]
 
 export default routes;
