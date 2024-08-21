da057c79c1a2320908e5020f50630aa00a299421
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-21 16:24:24 +0330

    created locations for location-page

---
+++ src/components/template/location/Locations/Locations.jsx
@@ -0,0 +1,69 @@
+import { FaSearch,  } from 'react-icons/fa'
+import './Locations.css'
+import { useState } from 'react'
+import Cards from '../Cards/Cards';
+
+
+function Locations() {
+    const [locationMenu ,  setLocationMenu ] = useState("coffee");
+
+    const handleMenuLocations = (menuID) =>{
+        setLocationMenu(menuID)
+    }
+
+  return (
+    <section className="locations">
+        <div className="containers">
+            <div className="locations_wrapper">
+                {/* locations search */}
+                <div className="locations_search">
+                    <span className="locations_search_icon">
+                        <FaSearch/>
+                    </span>
+                    <input type="text" className="locations_search_input" placeholder='جستجو کافه قهوه کده ...'/>
+                </div>
+                {/* locations search */}
+
+                <ul className="locations_menu">
+                    <li className="location_item">
+                        <button 
+                        className={locationMenu === "coffee" ?"location_btn_active" :"location_btn"}
+                        onClick={()=>handleMenuLocations("coffee")}
+                        >قهوه کده</button>
+                    </li>
+                    <li className="location_item">
+                        <button 
+                        className={locationMenu === "shop" ?"location_btn_active" :"location_btn"}
+                        onClick={()=>handleMenuLocations("shop")}
+                        >فروشگاه</button>
+                    </li>
+                </ul>
+
+                <div className="location_menu_site">
+                    {locationMenu === "coffee" &&(
+                        <>
+                            <Cards/>
+                            <Cards/>
+                            <Cards/>
+                            <Cards/>
+                            <Cards/>
+                        </>
+                    )}
+                    {locationMenu === "shop" &&(
+                        <>
+                            <Cards/>
+                            <Cards/>
+                            <Cards/>
+                            <Cards/>
+                            <Cards/>
+                            <Cards/>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Locations
--- src/page/location/Location.jsx
+++ src/page/location/Location.jsx
@@ -1,10 +1,12 @@
 import Breadcrumb from "../../components/modules/Breadcrumb/Breadcrumb"
+import Locations from "../../components/template/location/Locations/Locations"
 
 
 function Location() {
   return (
     <>
     <Breadcrumb title="نزدیکترین"/>
+    <Locations/>
     </>
   )
 }
