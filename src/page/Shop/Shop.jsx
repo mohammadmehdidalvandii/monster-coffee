53158982d7c0185195774cf8592e569634a85951
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-22 14:51:38 +0330

    created page-shop

---
+++ src/page/Shop/Shop.jsx
@@ -0,0 +1,12 @@
+import Breadcrumb from "../../components/modules/Breadcrumb/Breadcrumb"
+
+
+function Shop() {
+  return (
+    <>
+        <Breadcrumb title='فروشگاه'/>
+    </>
+  )
+}
+
+export default Shop
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import Home from "./page/Home/Home";
 import Location from "./page/location/Location";
+import Shop from "./page/Shop/Shop";
 
 const routes = [
     {path:"/" , element:<Home/>},
     {path:"/Location" , element:<Location/>},
+    {path:"/Shop" , element:<Shop/>},
 ]
 
 export default routes;
