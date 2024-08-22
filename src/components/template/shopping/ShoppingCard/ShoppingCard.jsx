a98e405e9cd35bc9ff3e7f0024868dedaaff1a53
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-22 17:03:58 +0330

    created section shoppningCard fot basket

---
+++ src/components/template/shopping/ShoppingCard/ShoppingCard.jsx
@@ -0,0 +1,34 @@
+import './ShoppingCard.css';
+import { MdDeleteForever } from 'react-icons/md';
+
+function ShoppingCard() {
+  return (
+    <div className="col-12">
+    <div className="shoppingCard">
+        <div className="shoppingCard_content">
+            <img src="/images/coffee1.jpg" alt="coffee" className="shoppingCard_img" />
+            <div className="shoppingCard_details">
+                <h6 className="shoppingCard_title">قهوه شکلاتی</h6>
+                <span className="shoppingCard_time">زمان اماده شدن - 16 دقیقه</span>
+                <span className="shoppingCard_price">40 تومان</span>
+            </div>
+        </div>
+        <div className="shoppingCard_action">
+            <button className="shoppingCard_remove">
+                <span className="shoppingCard_remove_text">حذف</span>
+                <span className="shoppingCard_remove_icon">
+                    <MdDeleteForever/>
+                </span>
+            </button>
+            <div className="shoppingCard_counter">
+                <button className="shoppingCard_countBtn">+</button>
+                    <span className="shoppingCard_count">1</span>
+                <button className="shoppingCard_countBtn">-</button>
+            </div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default ShoppingCard
