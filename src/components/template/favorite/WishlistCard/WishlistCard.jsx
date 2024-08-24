3d1cfb8e2cf306a907a59e27ad7513ead2f2f9a2
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-24 14:53:17 +0330

    created section wishlistCard for whishlist

---
+++ src/components/template/favorite/WishlistCard/WishlistCard.jsx
@@ -0,0 +1,34 @@
+import "./WishlistCard.css";
+import { Link } from 'react-router-dom';
+import { MdAddShoppingCart, MdDelete } from 'react-icons/md';
+
+function WishlistCard() {
+  return (
+    <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+      <Link to="/" className="wishlistCard">
+        <img
+          src="/images/coffee1.jpg"
+          alt="coffee"
+          className="wishlistCard_img"
+        />
+        <h5 className="wishlistCard_title">قهوه شکلاتی بخی</h5>
+        <div className="wishlistCard_buttons">
+          <button className="wishlistCard_btn">
+            <span className="wishlistCard_btnText">حذف</span>
+            <span className="wishlistCard_btnIcon">
+              <MdDelete />
+            </span>
+          </button>
+          <button className="wishlistCard_btn">
+            <span className="wishlistCard_btnText">افزودن</span>
+            <span className="wishlistCard_btnIcon">
+              <MdAddShoppingCart />
+            </span>
+          </button>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default WishlistCard;
