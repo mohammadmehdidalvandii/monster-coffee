95da7fc2573d43fda62a42cabc7b44a90a32580b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-26 17:14:35 +0330

    compalet section SignInOrSingUp

--- src/components/template/loginRegister/SignInOrSignUp/SignInOrSignUp.jsx
+++ src/components/template/loginRegister/SignInOrSignUp/SignInOrSignUp.jsx
@@ -1,6 +1,8 @@
 import "./SignInOrSignUp.css";
 import HeadTitle from "../../../../utils/HeadTitle";
 import { useState } from "react";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
 function SignInOrSignUp() {
   HeadTitle("ثبت نام | ورود ");
   const [menuForm, setMenuForm] = useState("Login");
@@ -16,22 +18,22 @@ function SignInOrSignUp() {
           <div className="col-12">
             {menuForm === "Login" ? (
               <button
-                className="btn"
+                className="loginRegister_btn"
                 onClick={() => handlerChangeMenu("Register")}
               >
                 ثبت نام
               </button>
             ) : (
               <button
-                className="btn"
+                className="loginRegister_btn"
                 onClick={() => handlerChangeMenu("Login")}
               >
                     ورود
               </button>
             )}
             
-            {menuForm === "Login" && (<h1>Login</h1>)}
-            {menuForm === "Register" && (<h1>register</h1>)}
+            {menuForm === "Login" && (<Login/>)}
+            {menuForm === "Register" && (<Register/>)}
           </div>
         </div>
       </div>
