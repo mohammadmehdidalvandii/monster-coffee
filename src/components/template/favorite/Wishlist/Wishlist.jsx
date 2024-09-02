f8874a65021eaaee314309e1b9daa0387e01d511
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-09-02 16:19:12 +0330

    crated get wishlist

--- src/components/template/favorite/Wishlist/Wishlist.jsx
+++ src/components/template/favorite/Wishlist/Wishlist.jsx
@@ -4,22 +4,27 @@ import './Wishlist.css';
 
 
 function Wishlist() {
+  const items = JSON.parse(localStorage.getItem("wishlist") || "[]");
+  
   return (
    <section className="wishlist">
         <div className="containers">
             <div className="row row-gap-4">
-                    <WishlistCard/>
-                    <WishlistCard/>
-                    <WishlistCard/>
-                    <WishlistCard/>
-                    <WishlistCard/>
 
-                    {/* <div className="col-12">
+              {items.length == 0 ?(      
+                 <div className="col-12">
                         <div className="wishlist_empty">
                                 <h5 className="wishlist_empty_title">مورد علاقه های شما خالی است !</h5>
                                 <Link to='/Shop' className='wishlist_empty_link'>سبد خرید </Link>
                         </div>
-                    </div> */}
+                    </div> 
+              ):(
+                items.map(item=>(
+                  <WishlistCard key={item.id} {...item}/>
+                ))
+              )}
+                   
+
             </div>
         </div>
    </section>
--- src/components/template/favorite/WishlistCard/WishlistCard.jsx
+++ src/components/template/favorite/WishlistCard/WishlistCard.jsx
@@ -2,16 +2,16 @@ import "./WishlistCard.css";
 import { Link } from 'react-router-dom';
 import { MdAddShoppingCart, MdDelete } from 'react-icons/md';
 
-function WishlistCard() {
+function WishlistCard({name , id}) {
   return (
     <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-      <Link to="/" className="wishlistCard">
+      <Link to={`/Shop-d/:${id}`} className="wishlistCard">
         <img
           src="/images/coffee1.jpg"
           alt="coffee"
           className="wishlistCard_img"
         />
-        <h5 className="wishlistCard_title">قهوه شکلاتی بخی</h5>
+        <h5 className="wishlistCard_title">{name}</h5>
         <div className="wishlistCard_buttons">
           <button className="wishlistCard_btn">
             <span className="wishlistCard_btnText">حذف</span>
