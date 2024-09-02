d83f525aa7468a8c0cf5c7ad5358133d022b276c
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-09-02 16:05:20 +0330

    created logic addToWishlist

--- src/components/template/shopDetails/Details/Details.jsx
+++ src/components/template/shopDetails/Details/Details.jsx
@@ -1,22 +1,42 @@
 import { FaHeart } from 'react-icons/fa';
 import './Details.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { showSwal } from '../../../../utils/Helpers';
 
 
 function Details({coffees}) {
     const [selectSize , SetSelectSize] = useState();
     const [price ,  setPrice] = useState()
+
+
     const handlerChangeSize = (event)=>{
         const sizeSelected = event.target.value;
         SetSelectSize(sizeSelected);
         const size = coffees.sizes.find((size) => size.size === sizeSelected);
         if (size) {
           setPrice(size.price)
+        }else{
+            setPrice(null)
         }
       }
-    useEffect(()=>{
-    },[selectSize , price])
 
+    //   add to coffee Wishlist
+    const addToWishlist = () => {
+            const wishlist = JSON.parse(localStorage.getItem("wishlist") || "[]");
+                const isInWishlist = wishlist.some((item)=> item.id === coffees.id) 
+                if(!isInWishlist){
+                    const newWishlist = {
+                        id :coffees.id , 
+                        name : coffees.name
+                    };
+
+                    wishlist.push(newWishlist)
+                    localStorage.setItem("wishlist" , JSON.stringify(wishlist))
+                    showSwal("محصول با موفقیت اضافه شد","success","فهمیدم")
+                }else{
+                    showSwal("محصول از قبل وجود دارد","error","متوجه شدم")
+                }
+      }
    
   return (
    <section className="details">
@@ -31,13 +51,13 @@ function Details({coffees}) {
                                     <h6 className="details_name">{coffees.name}</h6>
                                     <select name="" id="" className="details_menu" value={selectSize} onChange={handlerChangeSize}>
                                         <option className="details_option">انتخاب سایز</option>
-                                        {coffees?.sizes?.map((size)=>(
-                                            <option value={size.size} className="details_option" key={size}>{size.size}</option>
+                                        {coffees?.sizes?.map((size, index)=>(
+                                            <option value={size.size} className="details_option" key={index}>{size.size}</option>
                                         ))}
                                     </select>
                                     <span className="prices"> {price} تومان  </span> 
                                     <div className="detail_btns">
-                                        <button className="details_btn_favorite">
+                                        <button className="details_btn_favorite" onClick={addToWishlist}>
                                             <FaHeart/>
                                         </button>
                                         <button className="details_btn">
